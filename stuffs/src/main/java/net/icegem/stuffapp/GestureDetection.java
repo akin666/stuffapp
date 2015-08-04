69c6bb56b4a6b8ed09f485b112f19e904d5fd187
akin <mikael_korpela@hotmail.com>
2015-08-04 12:40:45 +0300

    somewhat working rotations

--- stuffs/src/main/java/net/icegem/stuffapp/GestureDetection.java
+++ stuffs/src/main/java/net/icegem/stuffapp/GestureDetection.java
@@ -12,8 +12,6 @@ import android.view.View;
  * http://stackoverflow.com/questions/10682019/android-two-finger-rotation
  */
 public class GestureDetection {
-    private static final int INVALID_POINTER_ID = -1;
-
     private Pan panDetection = new Pan();
     private Pinch pinchDetection = new Pinch();
     private Rotate rotateDetection = new Rotate();
@@ -23,18 +21,8 @@ public class GestureDetection {
     private PointF rawMiddle = new PointF(0,0);
 
     private int count = 0;
-/*
-    private PointF[] points = new PointF[10];
-    private int[] pointsID = new int[10];
-    private int iter = 0;
-*/
-    private float angle;
 
     private View view;
-
-    public float getAngle() {
-        return angle;
-    }
 
     public PointF getPoint() {
         return start;
@@ -53,8 +41,6 @@ public class GestureDetection {
     }
 
     public GestureDetection(View view) {
-        this.angle = 0.0f;
-
         this.view = view;
     }
 
@@ -118,129 +104,6 @@ public class GestureDetection {
                 break;
         }
 
-        /*
-        final PointF beta;
-        if( action == MotionEvent.ACTION_POINTER_UP) {
-            beta = getMiddle(event , event.getActionIndex());
-        } else {
-            beta = getMiddle(event);
-        }
-
-        // Move logic
-        switch(action) {
-            case MotionEvent.ACTION_POINTER_DOWN:
-            case MotionEvent.ACTION_POINTER_UP:
-                origin.set(beta.x - delta.x, beta.y - delta.y);
-                break;
-            case MotionEvent.ACTION_MOVE:
-                delta.set(beta.x - origin.x, beta.y - origin.y);
-                break;
-            case MotionEvent.ACTION_DOWN:
-                origin.set(beta);
-                delta.set(0 , 0);
-                if (listener != null) {
-                    listener.beginMove(this);
-                }
-                break;
-            case MotionEvent.ACTION_CANCEL:
-                delta.set(0 , 0);
-                if (listener != null) {
-                    listener.endMove(this);
-                }
-                break;
-            case MotionEvent.ACTION_UP:
-                if (listener != null) {
-                    listener.endMove(this);
-                }
-                delta.set(0 , 0);
-                break;
-            default:
-                break;
-        }
-
-        boolean assigned = false;
-        // Scale rotate logic
-        switch (action) {
-            case MotionEvent.ACTION_OUTSIDE:
-                break;
-            case MotionEvent.ACTION_DOWN:
-                ptrID1 = event.getPointerId(event.getActionIndex());
-                break;
-            case MotionEvent.ACTION_POINTER_DOWN:
-                if( ptrID1 == INVALID_POINTER_ID ) {
-                    assigned = true;
-                    ptrID1 = event.getPointerId(event.getActionIndex());
-                }
-                else if( ptrID2 == INVALID_POINTER_ID ) {
-                    assigned = true;
-                    ptrID2 = event.getPointerId(event.getActionIndex());
-                }
-
-                if (ptrID1 != INVALID_POINTER_ID && ptrID2 != INVALID_POINTER_ID && assigned) {
-                    getRawPoint(event, ptrID1, point1);
-                    getRawPoint(event, ptrID2, point2);
-
-                    scaleDistance = 1.0f / distanceBetweenPoints(point1, point2);
-
-                    if (listener != null) {
-                        listener.beginGesture(this);
-                    }
-                }
-                break;
-            case MotionEvent.ACTION_MOVE:
-                if (ptrID1 != INVALID_POINTER_ID && ptrID2 != INVALID_POINTER_ID){
-                    PointF tpoint1 = new PointF();
-                    PointF tpoint2 = new PointF();
-
-                    getRawPoint(event, ptrID1, tpoint1);
-                    getRawPoint(event, ptrID2, tpoint2);
-
-                    angle = angleBetweenLines(point1, point2, tpoint1, tpoint2);
-                    scale = scaleDistance * distanceBetweenPoints(tpoint1, tpoint2);
-                }
-                break;
-            case MotionEvent.ACTION_UP:
-                ptrID1 = INVALID_POINTER_ID;
-                ptrID2 = INVALID_POINTER_ID;
-                break;
-            case MotionEvent.ACTION_POINTER_UP:
-                if( event.getActionIndex() == ptrID1 ) {
-                    assigned = true;
-                    ptrID1 = INVALID_POINTER_ID;
-                }
-                if( event.getActionIndex() == ptrID2 ) {
-                    assigned = true;
-                    ptrID2 = INVALID_POINTER_ID;
-                }
-                if ((ptrID1 == INVALID_POINTER_ID || ptrID2 == INVALID_POINTER_ID) && assigned) {
-                    if (listener != null) {
-                        listener.endGesture(this);
-                    }
-                    scale = 1.0f;
-                    angle = 0.0f;
-                }
-                break;
-            case MotionEvent.ACTION_CANCEL:
-                ptrID1 = INVALID_POINTER_ID;
-                ptrID2 = INVALID_POINTER_ID;
-
-                scale = 1.0f;
-                angle = 0.0f;
-                if (listener != null) {
-                    listener.endGesture(this);
-                }
-                break;
-            default:
-                break;
-        }
-
-        if( action == MotionEvent.ACTION_MOVE ) {
-            if (listener != null) {
-                listener.onGesture(this);
-            }
-        }
-        */
-
         return true;
     }
 
@@ -293,33 +156,9 @@ public class GestureDetection {
         return new PointF((float) (length * Math.cos(Math.toRadians(angle))) + location[0] , (float) (length * Math.sin(Math.toRadians(angle))) + location[1]);
     }
 
-    void getRawPoint(MotionEvent ev, int index, PointF point){
-        final int[] location = { 0, 0 };
-        view.getLocationOnScreen(location);
-
-        float x = ev.getX(index);
-        float y = ev.getY(index);
-
-        double angle = Math.toDegrees(Math.atan2(y, x));
-        angle += view.getRotation();
-
-        final float length = PointF.length(x, y);
-
-        x = (float) (length * Math.cos(Math.toRadians(angle))) + location[0];
-        y = (float) (length * Math.sin(Math.toRadians(angle))) + location[1];
-
-        point.set(x, y);
-    }
-
-    private float angleBetweenLines(PointF fPoint, PointF sPoint, PointF nFpoint, PointF nSpoint)
-    {
-        float angle1 = (float) Math.atan2((fPoint.y - sPoint.y), (fPoint.x - sPoint.x));
-        float angle2 = (float) Math.atan2((nFpoint.y - nSpoint.y), (nFpoint.x - nSpoint.x));
-
-        float angle = ((float) Math.toDegrees(angle1 - angle2)) % 360;
-        if (angle < -180.f) angle += 360.0f;
-        if (angle > 180.f) angle -= 360.0f;
-        return -angle;
+    private float getAngle(float x , float y) {
+        float angle = (float) Math.toDegrees(Math.atan2(y - rawMiddle.y , x - rawMiddle.x));
+        return Helpers.clampAngle(angle);
     }
 
     public static class Rotate {
@@ -328,19 +167,6 @@ public class GestureDetection {
         private float[] trackAngle = new float[10];
         private float delta = 0.0f;
         private int iterator = 0;
-
-        private void swap( int a , int b ) {
-            if( a == b ) {
-                return;
-            }
-            int id = trackID[b];
-            float angle = trackAngle[b];
-
-            trackID[b] = trackID[a];
-            trackAngle[b] = trackAngle[a];
-            trackID[a] = id;
-            trackAngle[a] = angle;
-        }
 
         private void remove( int id ) {
             int index = findIDIndex( id );
@@ -365,6 +191,7 @@ public class GestureDetection {
 
         private void clear() {
             iterator = 0;
+            delta = 0.0f;
         }
 
         private int findIDIndex(int id) {
@@ -374,27 +201,6 @@ public class GestureDetection {
                 }
             }
             return -1;
-        }
-
-        private void setupAngle(PointF middle, float x, float y, int id, int index) {
-            float angle = (float) Math.toDegrees(Math.atan2(y - middle.y , x - middle.x));
-            if(angle < 0.0f){
-                angle += 360.0f;
-            }
-
-            trackAngle[index] = angle;
-            trackID[index] = id;
-        }
-
-        private void updateAngle(PointF middle, float x, float y, int index, float delta) {
-            float angle = getAngle( middle , x , y );
-
-
-        }
-
-        private float getAngle(PointF middle, float x, float y) {
-            float angle = (float) Math.toDegrees(Math.atan2(y - middle.y , x - middle.x));
-            return Helpers.clampAngle(angle);
         }
 
         public void cancel( GestureDetection detection , MotionEvent event ) {
@@ -416,22 +222,47 @@ public class GestureDetection {
 
                 // setup all points
                 for( int i = 0 ; i < detection.count ; ++i ) {
-                    float angle = getAngle( detection.rawMiddle , event.getX(i) , event.getY(i) );
-                    add( event.getPointerId(i) , Helpers.clampAngle(angle - delta) );
+                    add( event.getPointerId(i) , Helpers.clampAngle(detection.getAngle(event.getX(i), event.getY(i)) - delta) );
                 }
                 return;
             }
 
             int index = event.getActionIndex();
-            float angle = getAngle( detection.rawMiddle , event.getX(index) , event.getY(index) );
-            add( event.getPointerId(index) , Helpers.clampAngle(angle - delta) );
+            add( event.getPointerId(index) , Helpers.clampAngle(detection.getAngle(event.getX(index), event.getY(index)) - delta) );
         }
 
         public void move( GestureDetection detection , MotionEvent event ) {
+            float deltaCollector = 0.0f;
+
+            // update all existing angles
+            int counter = 0;
+            int count = event.getPointerCount();
+            for( int i = 0 ; i < count ; ++i ) {
+                int idx = findIDIndex(event.getPointerId(i));
+                if( idx < 0 )
+                {
+                    continue;
+                }
+                deltaCollector += Helpers.shortestDistanceAngle( trackAngle[idx] , detection.getAngle(event.getX(i), event.getY(i)) );
+                ++counter;
+            }
+
+            if( counter > 0 ) {
+                delta = deltaCollector / counter;
+            } else {
+                delta = 0.0f;
+            }
+
+            if( listener != null ) {
+                listener.onMove(this);
+            }
         }
 
         public void end( GestureDetection detection , MotionEvent event ) {
-            if( detection.count == 1 ) {
+            if( detection.count < 1 ) {
+                return;
+            }
+            else if( detection.count == 1 ) {
                 if( listener != null ) {
                     listener.onEnd(this);
                 }
@@ -449,9 +280,12 @@ public class GestureDetection {
                 if( i == index ) {
                     continue;
                 }
-            }
 
-            distance = getDistance(event) * (1.0f / scale);
+                int idx = findIDIndex( event.getPointerId(i) );
+                if( idx > 0 ) {
+                    trackAngle[idx] = Helpers.clampAngle(detection.getAngle(event.getX(i), event.getY(i)) - delta);
+                }
+            }
         }
 
         public float getDelta() {
